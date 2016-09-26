00d01fae48c03b8aaefa634922f1fcbf6d4d6ba8
serpent7776 <serpent7776@gmail.com>
2016-09-27 00:16:26 +0200

    added some more tests

--- src/test/java/com/github/serpent7776/gdx/foo/utils/RangesTest.java
+++ src/test/java/com/github/serpent7776/gdx/foo/utils/RangesTest.java
@@ -1,7 +1,11 @@
 package com.github.serpent7776.gdx.foo.utils;
 
+import java.util.Arrays;
+
 import org.testng.annotations.Test;
 import static org.testng.Assert.*;
+
+import com.badlogic.gdx.utils.Array;
 
 public class RangesTest {
 
@@ -13,4 +17,32 @@ public class RangesTest {
 			assertEquals(array.length, i);
 		}
 	}
+
+	@Test(expectedExceptions=IllegalArgumentException.class)
+	public void throwsOnZeroLengthParameter() {
+		Ranges.generatePermutedRange(0);
+	}
+
+	@Test(expectedExceptions=IllegalArgumentException.class)
+	public void throwsOnNegativeLengthParameter() {
+		Ranges.generatePermutedRange(-1);
+	}
+
+	@Test
+	public void returnedArrayHasNoDuplicates() {
+		final int maxLength = 250;
+		for (int testNo = 1; testNo <= maxLength; testNo++) {
+			final int[] array = Ranges.generatePermutedRange(testNo);
+			for (int number = 0; number < testNo; number++) {
+				int count = 0;
+				for (int i = 0; i < testNo; i++) {
+					if (array[i] == number) {
+						count++;
+					}
+				}
+				assertEquals(count, 1, "value " + number + " count is not 1. Array was " + Arrays.toString(array));
+			}
+		}
+	}
+
 }
